dff4c98693469ce8f744643294e269b009590248
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-30 11:52:31 +0100

    addded en superior class

--- src/AlgorithmsSorting/SortingAlgorithms.java
+++ src/AlgorithmsSorting/SortingAlgorithms.java
@@ -4,6 +4,7 @@ public class SortingAlgorithms {
     public static void main(String[] args) {
         System.out.println("Sup");
         System.out.println("8===>");
+        System.out.println("xX_VAPE NATION 420_xXx ");
     }
 
 }
