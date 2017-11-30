b7db7593331b5845ec0d83aa9cb0fada3dec5d5c
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-30 11:52:03 +0100

    addded en superior class

--- src/AlgorithmsSorting/SortingAlgorithms.java
+++ src/AlgorithmsSorting/SortingAlgorithms.java
@@ -3,5 +3,7 @@ package AlgorithmsSorting;
 public class SortingAlgorithms {
     public static void main(String[] args) {
         System.out.println("Sup");
+        System.out.println("8===>");
     }
+
 }
